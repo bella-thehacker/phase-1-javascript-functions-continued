6c2a56b28a22b19f782dd1e4b01a633fe8d10c71
Ivy Nyambura <ikagam018@gmail.com>
2024-06-27 21:12:35 +0300

    did functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){ 
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(work = "go to the office"){
+    return (`This Monday, I will ${work}.`);
+
+}
+console.log(mondayWork);
+console.log("work from home");
+
+function wrapAdjective(wrapper){
+return function(adjective){
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+};
+}
+const star = wrapAdjective("*");
+console.log(star("a hard worker"));
+
+const sticks = wrapAdjective("||");
+console.log(sticks("a dedicated programmer"));
+
